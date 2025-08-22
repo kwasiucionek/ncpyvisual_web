e6f85f4c989ac9629322e00b53c7ab919e4124df
kwasiucionek <kwasiek2gail.com>
2025-08-22 19:52:56 +0200

    add manual

--- app/__init__.py
+++ app/__init__.py
@@ -3,8 +3,7 @@
 NCPyVisual Web Application
 
 Aplikacja webowa do konfiguracji i testowania systemów rozpoznawania tablic rejestracyjnych.
-Obsługuje zarówno NCSim (symulacja) jak i NCShot (pełne przetwarzanie).
 """
 
-__version__ = "2.0.0"
-__author__ = "NCPyVisual Team"
+__version__ = "1.0.0"
+__author__ = "Krzysztof Wasiucionek"
